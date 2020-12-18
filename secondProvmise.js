3280d45e8e3da18c709c47519a2c30cf72fb02b9
Dmytro Nepyivoda <dimanepyivoda246@gmail.com>
2020-12-18 16:38:18 +0200

    lastPromise

--- secondProvmise.js
+++ secondProvmise.js
@@ -1,16 +1,17 @@
 const users=[
-    { name: 'Mango', active: true },
-    { name: 'Poly', active: false },
-    { name: 'Ajax', active: true },
-    { name: 'Lux', active: false },
-  ];
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
   
  function toggleUserState(allUsers,userName){
-   return Promise.resolve(allUsers.map(user =>user.name === userName?{ ...user, active: !user.active } : user))
+   return Promise.resolve(
+     allUsers.map(user =>user.name === userName?{ ...user, active: !user.active } : user))
   };
 
 
-   const logger = upDatedUsers => console.table(upDatedUsers)
+   const logger = upDatedUsers=>console.table(upDatedUsers)
   
   
   
